8db6fd330b81a45a9fd93265dd36df7760388062
Saketh Annimalla <sakethannimalla.09@gmail.com>
2023-11-12 18:46:49 -0600

    isSubsequence, consice soln

--- Leetcode75/two_pointers/002_is_subsequence/solution.py
+++ Leetcode75/two_pointers/002_is_subsequence/solution.py
@@ -5,15 +5,10 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        subStringPointer = 0
-        stringLength = len(t)
-        subStringLength = len(s)
-        if subStringLength == 0:
-            return True
-        for i in range(stringLength):
-            if subStringPointer<subStringLength and t[i] == s[subStringPointer]:
-                subStringPointer += 1
-            if subStringPointer == subStringLength:
-                return True
-        else:
-            return False
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
