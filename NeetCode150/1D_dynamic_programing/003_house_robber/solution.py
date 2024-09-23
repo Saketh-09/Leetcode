d2c6f5f49ac28a3f2e6eddeff0087921ee43db3e
saketh annimalla <sakethannimalla@gmail.com>
2024-09-22 23:47:12 -0500

    house robber

--- NeetCode150/1D_dynamic_programing/003_house_robber/solution.py
+++ NeetCode150/1D_dynamic_programing/003_house_robber/solution.py
@@ -4,8 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)>1:
-            nums[1] = max(nums[0],nums[1])
-        for i in range(2,len(nums)):
-            nums[i] = max(nums[i]+nums[i-2], nums[i-1])
-        return nums[-1]
+        res = []
+        r1,r2 = 0,0
+        for i in nums:
+            t = max(r1+i,r2)
+            r1 = r2
+            r2 = t
+        return r2
