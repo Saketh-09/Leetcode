89c96351ddb88f7b9e271db5a238dfb1038d8956
saketh annimalla <sakethannimalla@gmail.com>
2024-11-24 10:57:18 -0600

    Is same tree

---
+++ NeetCode150/trees/005_is_same_tree/solution.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        if (p and not q) or (q and not p):
+            return False
+        if p.val != q.val:
+            return False
+        l = self.isSameTree(p.left, q.left)
+        r = self.isSameTree(p.right, q.right)
+        return l and r
